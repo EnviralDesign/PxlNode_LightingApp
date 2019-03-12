a09a7c06dd5f2bdb241346859054509c6c0bdba0
SterlingRyan <ryanaquilina001@gmail.com>
2019-03-12 20:15:40 +0100

    Update about screen title

--- app/src/main/java/frost/com/homelighting/ui/about/AboutFragment.java
+++ app/src/main/java/frost/com/homelighting/ui/about/AboutFragment.java
@@ -4,28 +4,37 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import frost.com.homelighting.MainActivity;
 import frost.com.homelighting.R;
 
 public class AboutFragment extends Fragment {
-
-    private TextView mAboutText;
+    private MainActivity mainActivity;
 
     public static AboutFragment newInstance() {
         return new AboutFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mainActivity = (MainActivity) getActivity();
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_about, container, false);
-//        mAboutText = view.findViewById(R.id.about_text);
-//        mAboutText.setMovementMethod(LinkMovementMethod.getInstance());
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mainActivity.invalidateOptionsMenu();
+        mainActivity.setTitle(R.string.about_mode_title);
     }
 }
